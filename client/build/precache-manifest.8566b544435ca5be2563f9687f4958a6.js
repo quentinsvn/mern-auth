527547972910b52826a15ce0a6a4cd2f96aed91b
quenquen147 <quentin.savean@gmail.com>
2019-07-20 10:15:59 +0200

    Add files via upload

---
+++ client/build/precache-manifest.8566b544435ca5be2563f9687f4958a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4706ac89b3c8a34488f3d9d2f75185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b14219746c8c61d3f9d4",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "29123a89843b55a51dfe",
+    "url": "/static/js/2.6628fc2a.chunk.js"
+  },
+  {
+    "revision": "b14219746c8c61d3f9d4",
+    "url": "/static/js/main.f45c0c13.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ client/src/components/account/Account.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+
+class Account extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Mon compte</h1>
+            </div>
+        )
+    }
+}
+
+Account.propTypes = {
+    logoutUser: PropTypes.func.isRequired,
+    auth: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(
+    mapStateToProps,
+    { logoutUser }
+) (Account);
