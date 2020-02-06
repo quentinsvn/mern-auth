500672b41c847ec0d1bbbb276cc3207783b3e1ef
Quentin <quentin.savean@gmail.com>
2020-02-06 22:09:31 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://quenquen147:1593aze@cluster0-cd5mc.mongodb.net/test?retryWrites=true&w=majority",
+    mongoURI: "ADRESSE MONGODB",
     secretOrKey: "secret"
   };
